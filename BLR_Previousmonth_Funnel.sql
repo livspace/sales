201fd83a8cb74b62fef5d0e1a69c763d9e65e83a
yaswanthjk <47978386+yaswanthjk@users.noreply.github.com>
2019-02-27 17:27:13 +0530

    Create BLR_Previousmonth_Funnel.sql

---
+++ BLR_Previousmonth_Funnel.sql
@@ -0,0 +1,34 @@
+select 
+
+cm.name as "BM",
+sum(case when ps.weight>=110 then 1 else 0 end) as "Total Effective",
+sum(case when ps.weight>=250 then 1 else 0 end) as "Qualified",
+(sum(case when ps.weight>=250 then 1 else 0 end)/sum(case when ps.weight>=110 then 1 else 0 end))*100 as "Qualification %",
+sum(case when ps.weight >=270 then 1 else 0 end) as "Pitched",
+(sum(case when ps.weight >=270 then 1 else 0 end)/sum(case when ps.weight>=250 then 1 else 0 end))*100 as "Pitch to QL %",
+sum(case when ps.weight>=300 then 1 else 0 end) as "Converted",
+(sum(case when ps.weight>=300 then 1 else 0 end)/sum(case when ps.weight >=270 then 1 else 0 end))*100 as "Converted to Pitch %"
+
+
+
+from launchpad_backend.projects prs  
+
+
+
+join launchpad_backend.project_stages ps on ps.id=prs.stage_id
+
+left join launchpad_backend.project_settings pss on pss.project_id=prs.id and pss.is_deleted = 0 
+left join launchpad_backend.bouncer_users as cm on pss.primary_cm_id = cm.bouncer_id
+join launchpad_backend.cities ct on ct.id=prs.city_id
+
+where 
+
+((month(prs.created_at) = month(current_date())-1 and year(prs.created_at)=year(current_date()))
+or
+(month(prs.created_at) = month(current_date())+11 and year(prs.created_at)=year(current_date())-1))
+
+ and ct.name like "%bang%" and cm.name is not null
+
+group by cm.name 
+
+having sum(case when ps.weight>=110 then 1 else 0 end)>5
