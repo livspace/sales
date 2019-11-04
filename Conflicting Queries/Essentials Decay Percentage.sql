2e7794b1ca85929b48d7947583fa68b69753edeb
Pragyasingh8704 <55482800+Pragyasingh8704@users.noreply.github.com>
2019-11-04 16:16:53 +0530

    Create Essentials Decay Percentage.sql

---
+++ Conflicting Queries/Essentials Decay Percentage.sql
@@ -0,0 +1,27 @@
+--http://reports.livspace.com/question/2254
+
+Select 
+(case 
+when lower(project_city) like '%bangalore%' or lower(project_city) like '%hyderabad%' or lower(project_city) like '%chennai%' or lower(project_city) like '%pune%' then project_city
+when lower(project_city) like '%mumbai%' or lower(project_city) like '%thane%' then 'Mumbai'
+when lower(project_city) like '%gurgaon%' or lower(project_city) like '%dwarka%' then 'Gurgaon'
+when lower(project_city) like '%delhi%' or lower(project_city) like '%farida%' then 'Delhi'
+when lower(project_city) like '%noida%' or lower(project_city) like '%ghaz%' then 'Noida'
+end) as City,
+prs.primary_cm,
+prs.primary_designer,
+count(distinct(case when pvs.new_value=23 and pvs.event_date< CURRENT_DATE - INTERVAL '7 days' then prs.project_id end)) as BCS_Decay_count,
+count(distinct(case when pvs.new_value=23 then prs.project_id end)) as Total_BCS_Count,
+--(nullif(round(count(distinct(case when (pvs.new_value=23 and pvs.event_date< CURRENT_DATE - INTERVAL '7 days') then prs.project_id end)),0),2)/nullif(count(distinct(case when pvs.new_value=23 then prs.project_id end)),0)*100) as BCS_Decay_Percent,
+count(distinct(case when pvs.new_value=9 and pvs.event_date< CURRENT_DATE - INTERVAL '7 days' then prs.project_id end)) as BD_Decay_count,
+count(distinct(case when pvs.new_value=9 then prs.project_id end)) as Total_BD_Count
+--(nullif(round(count(distinct(case when (pvs.new_value=9 and pvs.event_date< CURRENT_DATE - INTERVAL '7 days') then prs.project_id end)),0),2)/nullif(count(distinct(case when pvs.new_value=9 then prs.project_id end)),0)*100) as BD_Decay_Percent
+
+
+from flat_tables.flat_projects prs
+left join flat_tables.flat_project_events pvs on prs.project_id=pvs.project_id 
+where pvs.event_date>=date_trunc('month',CURRENT_DATE) 
+and prs.primary_cm in ('Nidhi Agarwal','Anjuli Gupta','Abhishek Sharma','Palak Jolly','Rinkesh Bafna','Gautam Gupta','Sri Harsha Ks','Mohammed Azharuddin','Tarun Garg','Prateek Bharadwaj','Himanshi Sharma','Abhishek Sharma','Heli Kumar','Chandana  Ainwale','Annu  Bala','Natasha Sharon Shah','Natasha .','Satish Prasad  Sahu','Mohammad Takki','Shuchi',
+'Nitin Bhatia')
+
+group by 1,2,3
