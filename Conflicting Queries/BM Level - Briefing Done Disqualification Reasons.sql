e156e80b5c839d9b2c7ee0d4819fc4c994851dd2
Pragyasingh8704 <55482800+Pragyasingh8704@users.noreply.github.com>
2019-11-05 09:58:20 +0530

    Create BM Level - Briefing Done Disqualification Reasons.sql

---
+++ Conflicting Queries/BM Level - Briefing Done Disqualification Reasons.sql
@@ -0,0 +1,43 @@
+--http://reports.livspace.com/question/1857
+
+select 
+
+fprs.primary_cm,
+to_char(prs.created_at,'YYYY-MM') as month,
+sum(case when ps.weight>=110 then 1 else 0 end) as Total_Effective,
+
+sum(case when prs.status = 'INACTIVE' then 1 else 0 end) as Disqualified,
+(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end)/nullif(sum(case when ps.weight>=110 then 1.0 else 0 end),0))*100 as Disqualified_Percent,
+
+(sum(case when prs.inactivation_reason = 'posted_by_mistake' then 1.0 else 0 end)/nullif(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end),0)*100) as Posted_By_Mistake_Percent,
+
+(sum(case when lower(prs.inactivation_reason) = 'rnr' then 1.0 else 0 end)/nullif(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end),0)*100) as RNR_Percent,
+
+(sum(case when prs.inactivation_reason = 'budget' then 1.0 else 0 end)/nullif(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end),0)*100) as budget_Percent,
+
+(sum(case when prs.inactivation_reason = 'client_doesnot_want_to_work_with_us' then 1.0 else 0 end)/nullif(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end),0)*100) 
+as client_does_not_want_Percent,
+
+(sum(case when prs.inactivation_reason = 'change_of_plan' then 1.0 else 0 end)/nullif(sum(case when prs.status = 'INACTIVE' then 1.0 else 0 end),0)*100) as change_of_plan_Percent
+
+
+from launchpad_backend.projects prs  
+
+join launchpad_backend.project_stages ps on ps.id=prs.stage_id
+join launchpad_backend.cities ct on ct.id=prs.city_id
+left join (select project_id,primary_cm from flat_tables.flat_projects group by project_id,primary_cm) fprs on fprs.project_id=prs.id
+
+
+where ct.name like '%chennai%'
+
+and prs.created_at > to_date('01 Sep 2018','DD Mon YYYY')
+
+and ps.weight>=110 
+
+and ps.weight>=250 
+
+and ps.weight<270 
+
+group by 1,2
+
+having sum(case when ps.weight>=110 then 1 else 0 end)>5
